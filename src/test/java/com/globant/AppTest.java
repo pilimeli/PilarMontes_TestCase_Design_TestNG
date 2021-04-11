bf3337916b4ad3798ae8b7740217352d776921fe
pilimeli <82001658+pilimeli@users.noreply.github.com>
2021-04-11 15:36:27 -0500

    Actualizacion solucion Taller1

--- src/test/java/com/globant/AppTest.java
+++ src/test/java/com/globant/AppTest.java
@@ -1,20 +1,61 @@
 package com.globant;
 
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
+import org.junit.After;
+import org.testng.annotations.*;
+import static org.testng.Assert.assertTrue;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
+
     /**
      * Rigorous Test xyz :-)
      */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+
+    @BeforeSuite
+    public void cuentaEspnCreada() {
+        System.out.println("Poseer Cuenta ESPN");
     }
+
+    @BeforeMethod
+    public void ingresarUseryPass(){
+        System.out.println("Abrir el Navegador: https://www.espn.com/?src=com&adblock=true");
+        System.out.println("Ingresar con usuario y contraseña");
+
+    }
+    @AfterMethod
+    public void finalDePrueba(){
+        System.out.println("Cerrar Navegador");
+        System.out.println("-------------------------");
+    }
+    private void clicInciarSesion (){
+        System.out.println("clic en iniciar sesion");
+    }
+
+    @Test(priority=0)
+    public void iniciarSesion()
+    {
+        clicInciarSesion();
+        assertTrue( true );
+        System.out.println("Iniciar Sesion");
+    }
+    @Test (priority=1)
+    public void cerrarSesion()
+    {
+        clicInciarSesion();
+        assertTrue( true );
+        System.out.println("Cerrar Sesion");
+    }
+    @Test(priority=2)
+    public void desactivarSesion()
+    {
+        clicInciarSesion();
+        assertTrue( true );
+        System.out.println("Clic en ESPN Profile");
+        System.out.println("Clic en Delete Account");
+        System.out.println("Seleccionar YES,delete this account");
+    }
+
 }
